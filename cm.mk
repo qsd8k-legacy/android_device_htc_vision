7329be4daf556a8a795c7ff9878e182f2cdb7572
Vladislav Koldobskiy <admin@nevergone.ru>
2013-12-14 14:56:45 +0400

    Update fingerprint & build_desc

--- cm.mk
+++ cm.mk
@@ -6,7 +6,8 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vision BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:4.2.2/JDQ39/87995:user/release-keys PRIVATE_BUILD_DESC="4.2.2 Andromadus release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vision BUILD_FINGERPRINT="htc_wwe/htc_vision/vision:$(PLATFORM_VERSION)/$(BUILD_ID)/$(shell date +%Y%m%d%H%M%S):$(TARGET_BUILD_VARIANT)/test-keys" PRIVATE_BUILD_DESC="htc_vision-$(TARGET_BUILD_VARIANT) $(PLATFORM_VERSION) $(BUILD_ID) $(shell date +%Y%m%d%H%M%S) test-keys"
+
 
 PRODUCT_NAME := cm_vision
 PRODUCT_DEVICE := vision
