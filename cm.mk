090e25623434e953dd0e142bcfd26b8c0ae58d32
Micha LaQua <micha.laqua@gmail.com>
2014-08-20 22:47:34 +0200

    use generic fingerprint format

--- cm.mk
+++ cm.mk
@@ -6,8 +6,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vision BUILD_FINGERPRINT="htc_wwe/htc_vision/vision:$(PLATFORM_VERSION)/$(BUILD_ID)/$(shell date +%Y%m%d%H%M%S):$(TARGET_BUILD_VARIANT)/test-keys" PRIVATE_BUILD_DESC="htc_vision-$(TARGET_BUILD_VARIANT) $(PLATFORM_VERSION) $(BUILD_ID) $(shell date +%Y%m%d%H%M%S) test-keys"
-
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vision BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:4.4.4/KTU84P/1227136:user/release-keys PRIVATE_BUILD_DESC="vision-user 4.4.4 KTU84P 1227136 release-keys"
 
 PRODUCT_NAME := cm_vision
 PRODUCT_DEVICE := vision
